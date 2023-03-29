3cd8713153b69812eba49050ff9b39dbea553934
Paulo Riveiro <pauloriveiro01@gmail.com>
2023-03-29 01:49:13 +0000

    Almost finished needs events(adding Red Hearts and diamonds)

--- src/app.js
+++ src/app.js
@@ -8,4 +8,36 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let valueCard = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let symbolCard = ["♦", "♥", "♠", "♣"];
+
+  function randomizer(array) {
+    return array[Math.floor(Math.random() * array.length)]; // 0 - 3
+  }
+
+  let cardValue = `${randomizer(symbolCard)} ${randomizer(
+    valueCard
+  )} ${randomizer(symbolCard)}`;
+
+  console.log(cardValue);
+
+  let comp = (document.getElementById("up").innerHTML = `${randomizer(
+    symbolCard
+  )}`);
+  document.getElementById("center").innerHTML = `${randomizer(valueCard)}`;
+  document.getElementById("down").innerHTML = `${comp}`;
 };
